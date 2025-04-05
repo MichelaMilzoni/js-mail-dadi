3755fa9a7497790c1939f12ee10ac0895824a3a2
MichelaMilzoni <michelamilzoni0@gmail.com>
2025-04-05 16:37:44 +0200

    bonus dadi: aggiunto pareggio

--- dadi/dadi.js
+++ dadi/dadi.js
@@ -6,16 +6,12 @@
 // // Se dobbiamo confrontare qualcosa che "cosa" ci serve?
 
 
-// creo variabili con valore 0
-let randomNumber;
 
 // genero numero random da 1 a 6
-
 // Lancio giocatore
 const userRandomNumber =  Math.floor(Math.random() * 6) + 1;
 // Lancio del PC
 const pcRandomNumber = Math.floor(Math.random() * 6) + 1;
-
 
 alert(`Numero del giocatore: ${userRandomNumber}`)
 console.log(`Numero del giocatore: ${userRandomNumber}`); // ${numero} lo uso per "unire" stringa e numero
@@ -25,7 +21,10 @@ console.log(`Numero del PC: ${pcRandomNumber}`);
 if (userRandomNumber > pcRandomNumber) {
     alert("Complimenti hai vinto!")
     console.log("User vince");    
-} else {
+} else if (pcRandomNumber > userRandomNumber) {
     alert("Peccato, hai perso!")
     console.log("Pc vince");    
+} else {
+    alert("È un pareggio!");
+    console.log("Pareggio");
 }
